30ae65e6785f05d133ddaecfdd246962b9110b5b
Adriano Vieira <adriano.savoir@gmail.com>
2020-10-24 15:59:15 -0300

    ajustes ip + healthcheck

--- src/test/java/br/com/amedigital/tasks/functional/TasksTest.java
+++ src/test/java/br/com/amedigital/tasks/functional/TasksTest.java
@@ -8,7 +8,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -17,8 +16,8 @@ public class TasksTest {
 	public WebDriver acessarAplicacao() throws MalformedURLException {
 //		WebDriver driver = new ChromeDriver();
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.10:4444/wd/hub"), cap);
-		driver.navigate().to("http://192.168.0.10:8001/tasks");
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.4:4444/wd/hub"), cap);
+		driver.navigate().to("http://192.168.0.4:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
