ccbc7122e08f3a8724b329fba07e2cc215d7202b
Adriano Vieira <adriano.savoir@gmail.com>
2020-10-27 23:24:01 -0300

    ajjuste em teste de remoção de tarefa

--- src/test/java/br/com/amedigital/tasks/functional/TasksTest.java
+++ src/test/java/br/com/amedigital/tasks/functional/TasksTest.java
@@ -131,12 +131,12 @@ public class TasksTest {
 			driver.findElement(By.id("dueDate")).sendKeys("21/12/2040");
 			driver.findElement(By.id("saveButton")).click();
 			String message = driver.findElement(By.id("message")).getText();
-			Assert.assertEquals("Success!", message);
+			Assert.assertEquals("Success!!", message);
 			
 			// remover a tarefa
 			driver.findElement(By.xpath("//a[@class='btn btn-outline-danger btn-sm']")).click();
 			message = driver.findElement(By.id("message")).getText();
-			Assert.assertEquals("Success!", message);
+			Assert.assertEquals("Success!!", message);
 			
 		} finally {
 			// fechar o browser
